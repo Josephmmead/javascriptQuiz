ae95b9fbddcf0a63f0e9a318fdebbfd75c9ebe54
Joseph Mead <josephmmead@gmail.com>
2020-09-21 18:26:58 -0400

    added a reset css, as well as updated some css/javascript

--- js/logic.js
+++ js/logic.js
@@ -1,36 +1,96 @@
-var currentQuestionIndex = 0
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        choices: ["Hot Tub Malfunction Light", "Hypertext Markup Language", "Hot Text Manipulation Lanuage", "Hotel Teleport Marketing Language"],
+        answer: "Hypertext Markup Language",
+    },
+    {
+        question: "questions #2",
+        choices: ["Java", "Ruby", "Python", "Javascript"],
+        answer: "Javascript",
+    },
+    {
+        question: "questions #3",
+        choices: ["Java", "Ruby", "Python", "Javascript"],
+        answer: "Javascript",
+    },
+    {
+        question: "questions #4",
+        choices: ["Java", "Ruby", "Python", "Javascript"],
+        answer: "Javascript",
+    },
+
+]
 
 
+var currentQuestionIndex = 0;
+var timer = $("#time");
+var score = 0;
+var currentAnswer = questions[currentQuestionIndex].answer;
+let selection;
+
+
+
+$("#start").on("click", function(){
+    startQuiz();   
+});
 
 function startQuiz() {
-    var startScreenEl = document.getElementById("startScreen")
-
+    var startScreenEl = document.getElementById("startScreen");
     // hide start screen once start button is clicked
+    
+    startScreenEl.classList.add('hide');
 
     // start timer upon clicking "Start Quiz"
 
-    getQuestion()
-}
+    getQuestion();
+   
+};
 
 function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex]
-    var currentChoices = questions[currentQuestionIndex].choices
+    $('#questionHeader').empty();
+    $('#choices').empty();
 
-    var titleElement = document.getElementById("question-title")
-    titleElement.textContent = currentQuestion.question
+    var currentQuestion = questions[currentQuestionIndex].question;
+    var currentChoices = questions[currentQuestionIndex].choices;
+    currentAnswer = questions[currentQuestionIndex].answer
 
-    var choicesElement = document.getElementById("choices")
-    console.log(currentChoices)
+    var headerElement = document.getElementById("questionHeader");
+    headerElement.textContent = currentQuestion.question;
 
-   for(var i = 0; i < currentChoices.length; i++) {
-        var choiceSelection = document.createElement("button")
-        choiceSelection.setAttribute("class", "choice")
-        choiceSelection.setAttribute("value", currentChoices[i])
+    var choicesElement = document.getElementById("choices");
+    
+    
+   for( i = 0; i < currentChoices.length; i++) {
+    var choiceSelection = document.createElement("button");
+        choiceSelection.setAttribute("class", "choice-btn");
+        choiceSelection.setAttribute("value", currentChoices[i]);
 
-        choiceSelection.textContent = i + 1 + ". " + currentChoices[i]
+        choiceSelection.textContent = i + 1 + ". " + currentChoices[i];
 
-        choicesElement.appendChild(choiceSelection)
-    }
-}
+        
 
-// Here is where you will define an onclick to run the startQuiz function
+        choicesElement.appendChild(choiceSelection);
+        
+    };
+    headerElement.append(currentQuestion);
+    
+    
+};
+// $(document).on()"click",".end-btn", function(){
+
+$(document).on("click",".choice-btn", function(){
+    selection = this.value;
+
+    if (selection !== currentAnswer) {
+        alert("Wrong");
+        currentQuestionIndex= currentQuestionIndex + 1;
+        getQuestion();
+        
+    } else {
+        score = score + 10;
+        alert("Correct! Your current score is " +score)
+        currentQuestionIndex= currentQuestionIndex + 1;
+        getQuestion(); };
+
+});
